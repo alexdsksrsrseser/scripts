4da77b7338da58d802958818bf9f4180924a60f9
alexdsksrsrseser <rsindian510@gmail.com>
2026-05-04 21:24:56 -0600

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,146 @@
+
+//
+// created by AheadLib
+// github:https://github.com/strivexjun/AheadLib-x86-x64
+//
+
+#include <windows.h>
+#include <Shlwapi.h>
+
+#pragma comment( lib, "Shlwapi.lib")
+
+#pragma comment(linker, "/EXPORT:DllMain=AheadLib_DllMain,@1")
+#pragma comment(linker, "/EXPORT:MessageBoxThread=AheadLib_MessageBoxThread,@2")
+
+
+extern "C" 
+{
+PVOID pfnAheadLib_DllMain;
+PVOID pfnAheadLib_MessageBoxThread;
+}
+
+
+static
+HMODULE g_OldModule = NULL;
+
+VOID WINAPI Free()
+{
+	if (g_OldModule)
+	{
+		FreeLibrary(g_OldModule);
+	}
+}
+
+
+BOOL WINAPI Load()
+{
+	TCHAR tzPath[MAX_PATH];
+	TCHAR tzTemp[MAX_PATH * 2];
+
+	//
+	// ??????????????????DLL
+	//
+	//GetModuleFileName(NULL,tzPath,MAX_PATH); //???????
+	//PathRemoveFileSpec(tzPath);
+
+	GetSystemDirectory(tzPath, MAX_PATH); //?????????
+
+	lstrcat(tzPath, TEXT("\\a.dll"));
+
+	g_OldModule = LoadLibrary(tzPath);
+	if (g_OldModule == NULL)
+	{
+		wsprintf(tzTemp, TEXT("?????? %s,????????"), tzPath);
+		MessageBox(NULL, tzTemp, TEXT("AheadLib"), MB_ICONSTOP);
+	}
+
+	return (g_OldModule != NULL);
+
+}
+
+
+FARPROC WINAPI GetAddress(PCSTR pszProcName)
+{
+	FARPROC fpAddress;
+	CHAR szProcName[64];
+	TCHAR tzTemp[MAX_PATH];
+
+	fpAddress = GetProcAddress(g_OldModule, pszProcName);
+	if (fpAddress == NULL)
+	{
+		if (HIWORD(pszProcName) == 0)
+		{
+			wsprintfA(szProcName, "#%d", pszProcName);
+			pszProcName = szProcName;
+		}
+
+		wsprintf(tzTemp, TEXT("?????? %hs,????????"), pszProcName);
+		MessageBox(NULL, tzTemp, TEXT("AheadLib"), MB_ICONSTOP);
+		ExitProcess(-2);
+	}
+	return fpAddress;
+}
+
+BOOL WINAPI Init()
+{
+	pfnAheadLib_DllMain = GetAddress("DllMain");
+	pfnAheadLib_MessageBoxThread = GetAddress("MessageBoxThread");
+	return TRUE;
+}	
+
+DWORD WINAPI ThreadProc(LPVOID lpThreadParameter)
+{
+	HANDLE hProcess;
+
+	PVOID addr1 = reinterpret_cast<PVOID>(0x00401000);
+	BYTE data1[] = { 0x90, 0x90, 0x90, 0x90 };
+
+	//
+	// ??VMP3.x ?????
+	//
+	hProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, GetCurrentProcessId());
+	if (hProcess)
+	{
+		WriteProcessMemory(hProcess, addr1, data1, sizeof(data1), NULL);
+
+		CloseHandle(hProcess);
+	}
+
+	return 0;
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, PVOID pvReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(hModule);
+
+		if (Load() && Init())
+		{
+			TCHAR szAppName[MAX_PATH] = TEXT("MyApp.exe");//????????
+			TCHAR szCurName[MAX_PATH];
+
+			GetModuleFileName(NULL, szCurName, MAX_PATH);
+			PathStripPath(szCurName);
+
+			//?????????
+			if (StrCmpI(szCurName, szAppName) == 0)
+			{
+				//????????????
+				HANDLE hThread = CreateThread(NULL, NULL, ThreadProc, NULL, NULL, NULL);
+				if (hThread)
+				{
+					CloseHandle(hThread);
+				}
+			}
+		}
+	}
+	else if (dwReason == DLL_PROCESS_DETACH)
+	{
+		Free();
+	}
+
+	return TRUE;
+}
+
